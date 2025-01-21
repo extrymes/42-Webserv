8486938d054618894d80ba489001416daf4cdf13
extrymes <65893656+extrymes@users.noreply.github.com>
2025-01-21 23:35:26 +0100

    chore: update webserv.hpp

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -3,28 +3,9 @@
 # include <fstream>
 # include <sstream>
 # include <exception>
+# include "Config.hpp"
 # include "colors.h"
 
-// --- Structures ---
-typedef struct data data;
-typedef struct s_config t_config;
-
-struct s_config {
-	std::string serverHost;
-	std::string serverName;
-	int serverPort;
-	std::string *errorPages;
-	int maxClientBodySize;
-};
-
-
-// --- Parsing ---
-// Reader
-void	readFile(std::string filename);
-
-// Utils
-void	trim(std::string &str);
-
 // socket
-void	handleSocket(t_config serverConfig, t_socket &socketConfig);
-void	handleSocketClient(t_socket &socketConfig, t_config serverConfig);
+void handleSocket(t_config serverConfig, t_socket &socketConfig);
+void handleSocketClient(t_socket &socketConfig, t_config serverConfig);
