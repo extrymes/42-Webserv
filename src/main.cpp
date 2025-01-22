bb442b085dd86711329428c08e6d80cd6ab10ec9
extrymes <65893656+extrymes@users.noreply.github.com>
2025-01-22 07:06:13 +0100

    chore: update source files

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@ int main(int ac, char **av) {
 		std::cerr << RED "Usage:\n\t./webserv <config_file>" RESET << std::endl;
 		return 1;
 	}
-	t_config serverConfig;
+	t_config config;
 	t_socket socketConfig;
 	try {
-		readFile(av[1]);
-		handleSocket(serverConfig, socketConfig);
+		Config::parseConfigFile(av[1], config);
+		handleSocket(config, socketConfig);
+		for (std::vector<t_server>::iterator it = config.servers.begin(); it != config.servers.end(); ++it)
+			std::cout << GREEN << "Server name: " << it->name << std::endl;
 	}
-	catch(const std::exception& e) {
+	catch (const std::exception& e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
 	}
 	return 0;
