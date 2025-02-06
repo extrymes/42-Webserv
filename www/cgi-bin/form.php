247365e6317428500d013b5c372d85508de28b99
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-06 09:05:29 +0100

    feat(CGI): parse body in form.php

--- www/cgi-bin/form.php
+++ www/cgi-bin/form.php
@@ -1,13 +1,32 @@
-#!usr/bin/php
+#!/usr/bin/php
 
 <?php
 
 // Get form data
-$name = isset($_GET["name"]) ? $_GET["name"] : "Guest";
-$message = isset($_GET["message"]) ? $_GET["message"] : "No message";
+$body = getenv("body");
+if (!$body) {
+	echo "<html><body>";
+	echo "<h1>Simple Form</h1>";
+	echo "<p>name: Unknown</p>";
+	echo "<p>message: No message</p>";
+	echo "</html></body>";
+	exit();
+}
 
-// Define header
-header("Content-Type: text/html");
+# Parse body into dictionary
+$data_dict = [];
+$splitted_body = explode("&", $body);
+foreach ($splitted_body as $item) {
+	$pair = explode("=", $item, 2);
+	if (count($pair) === 2) {
+		list($key, $value) = $pair;
+		$data_dict[urldecode($key)] = urldecode($value);
+	}
+}
+
+// Get each data
+$name = $data_dict["name"];
+$message = $data_dict["message"];
 
 // Print HTML
 echo "<html><body>";
