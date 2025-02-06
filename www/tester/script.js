a5a52bb5e9043ba28fc1672833caaa8be3a1e773
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-06 16:06:37 +0100

    feat(www): add methods tester

---
+++ www/tester/script.js
@@ -0,0 +1,28 @@
+function setMethod(method) {
+	document.getElementById("form").method = method;
+}
+
+function setMethod2(method) {
+	document.getElementById("form2").method = method;
+}
+
+function deleteFile() {
+	const filePath = document.getElementById("filename").value;
+
+	fetch(filePath, {
+		method: 'DELETE'
+	})
+	.then((response) => {
+		if (response.redirected)
+			return window.location.href = response.url;
+		if (response.status === 204)
+			return alert("File deleted!");
+		if (response.status === 404)
+			return alert("Failed to delete file!");
+		return response.text().then(html => {
+			document.open();
+			document.write(html);
+			document.close();
+		});
+	});
+}
