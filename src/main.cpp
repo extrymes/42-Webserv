b027a974e06c4b55a368696f9942009f14a8a3bd
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-11 16:57:42 +0100

    fix: use a default config file if not specified

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,22 @@
 #include "socket.hpp"
 
 int main(int ac, char **av) {
-	if (ac != 2) {
+	if (ac > 2) {
 		std::cerr << "Usage:\n\t./webserv <config_file>" << std::endl;
 		return 1;
 	}
+	std::string configFile = "webserv.conf";
+	if (ac == 2)
+		configFile = av[1];
 	std::signal(SIGPIPE, SIG_IGN);
 	std::vector<t_server> servers;
 	t_socket socketConfig;
 	try {
-		ParseConfig(av[1], servers);
+		ParseConfig(configFile, servers);
 		handleSocket(servers, socketConfig);
 	} catch (const std::exception& e) {
 		std::cerr << "webserv: error: " << e.what() << std::endl;
+		return 1;
 	}
 	return 0;
 }
